f91670e277898e506d4059fe1de8408a6a03d1f8
Robert Merrill <robgmerrill@gmail.com>
2020-04-29 10:09:58 -0700

    Single meal styling and display random meal

--- script.js
+++ script.js
@@ -60,6 +60,20 @@ function getMealById(mealID) {
     });
 }
 
+// Fetch random meal from API
+function getRandomMeal() {
+  // clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      addMealToDOM(meal);
+    });
+}
+
 // Add meal to DOM
 function addMealToDOM(meal) {
   const ingredients = [];
@@ -95,6 +109,8 @@ function addMealToDOM(meal) {
 
 // Event listeners
 submit.addEventListener('submit', searchMeal);
+
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', (e) => {
   const mealInfo = e.path.find((item) => {
